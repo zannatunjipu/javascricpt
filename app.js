d885d83f2da66b55093b70f5ff6ed96300802807
zannatunjipu <zannatunjipu@gmail.com>
2024-12-22 14:35:23 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+
+
+// function number (a,b){
+//     for (a; a<=b; a=a+1){
+//         console.log (a);
+//     }
+// }
+//  number (1,100)
+//  number (1,5)
+
+
+// function SUM (a,b) {
+//     let sum = a*b 
+//     console.log(a,"x",b,"=",sum);
+// }
+
+// SUM(34524,64)
+// SUM(56,9)
+ 
+// let names = [
+//     "jipu", "jame", "jesmin", "leon"
+// ]
+// let e = names.length -1
+// let i = 0
+// while(i<=e){
+//     console.log(i,".",names[i]);
+//     i=i+1
+// }
+
+
+// let nums = [1,3,4,6,8,12,35,13,13]
+// let a = nums.length
+// console.log(a)
+
+// let nums = [1,4,7,46,3435,89]
+// let a = Math.max(...nums)
+// console.log("Max number",a);
+
+// let nums = [1,4,7,46,3435,89]
+// let a = Math.min(...nums)
+// console.log("Min number",a);
+
+// let password = 55555
+// if ( password=="55548"){
+//     console.log ( "login done");
+// }
+// else {
+//     console.log (" wrong ! password");
+// // }
+// let login = document.getElementById('txt')
+// let phone_number = prompt ("phone number:")
+// let pin = prompt ("password: ")
+
+// if ( phone_number =="01310887411" && pin == "5548"){
+//     login.innerText = 'login done '
+
+// }
+// else{ 
+//     login.innerText = 'Sorry!'
+// }
+
+// var messi = 7
+// var ronaldo = 10 
+
+// [messi,ronaldo]=[ronaldo,messi]
+// console.log("messi:", messi);
+
+let friend = [ 'jipu','jame', 'jesmin']
+console.log(friend );
